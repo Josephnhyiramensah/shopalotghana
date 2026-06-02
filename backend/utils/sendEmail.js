29c6bc4a5ec62fcbdd7fb673e73b41395f23a0c4
Joseph Nhyira Mensah <josephnhyiramensah@gmail.com>
2026-06-02 14:05:36 -0300

    Remove test email file

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,28 +1,27 @@
-import nodemailer from "nodemailer"
 import process from "node:process"
+import nodemailer from "nodemailer"
 
-const sendEmail = async function({ to, subject, html }) {
+const sendEmail = async function(options) {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host:   "smtp-relay.brevo.com",
+    port:   587,
+    secure: false,
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      user: process.env.BREVO_USER,
+      pass: process.env.BREVO_PASS,
     },
   })
 
-  console.log("Sending email to:", to)
-  console.log("Using Gmail:", process.env.EMAIL_USER)
-
   const mailOptions = {
-    from: '"Shopalotghana" <' + process.env.EMAIL_USER + '>',
-    to: to,
-    subject: subject,
-    html: html,
+    from:    '"Shopalotghana" <' + process.env.BREVO_USER + ">",
+    to:      options.to,
+    subject: options.subject,
+    html:    options.html,
   }
 
-  await transporter.sendMail(mailOptions)
-  console.log("Email sent successfully!")
+  const info = await transporter.sendMail(mailOptions)
+  console.log("Email sent:", info.messageId)
+  return info
 }
 
 export default sendEmail
-
